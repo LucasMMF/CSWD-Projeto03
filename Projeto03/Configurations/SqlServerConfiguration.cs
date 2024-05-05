4b51db795f076040166af7be65472adcd4437f95
LucasMMF <mateusmidao@gmail.com>
2024-05-05 15:59:44 -0300

    feat: adding repositories and config class
    adds ProdutoRepository as well as an
    SqlServer configuration class, that returns
    the value of the ConnectionString needed
    for the database access.

---
+++ Projeto03/Configurations/SqlServerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03.Configurations
+{
+    public class SqlServerConfiguration
+    {
+        /// <summary>
+        /// Método para retornar o endereço
+        /// da ConnectionString do banco de dados
+        /// </summary>
+        public static string GetConnectionString()
+        {
+            return @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDProjeto03;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        }
+    }
+}
---
+++ Projeto03/Repositories/ProdutoRepository.cs
@@ -0,0 +1,64 @@
+﻿using Dapper;
+using Projeto03.Configurations;
+using Projeto03.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03.Repositories
+{
+    public class ProdutoRepository
+    {
+        public void Create(Produto produto)
+        {
+            // Escrevendo um comando SQL para gravar um produto na tabela
+            var query = @"
+                INSERT INTO PRODUTO(
+                    IDPRODUTO,
+                    NOME,
+                    PRECO,
+                    QUANTIDADE,
+                    DATACADASTRO)
+                VALUES(
+                    NEWID(),
+                    @Nome,
+                    @Preco,
+                    @Quantidade,
+                    GETDATE())
+            ";
+
+            // Conectando no banco de dados
+            using (var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))
+            {
+                // Executando o comando SQL no banco
+                // passando os dados do objeto
+                connection.Execute(query, produto);
+            }
+        }
+
+        public void Update(Produto produto)
+        {
+            // TODO
+        }
+
+        public void Delete(Produto produto) 
+        {
+            // TODO
+        }
+
+        public List<Produto> GetAll()
+        {
+            // TODO
+            return null;
+        }
+
+        public Produto GetById(Guid idProduto)
+        {
+            // TODO
+            return null;
+        }
+    }
+}
